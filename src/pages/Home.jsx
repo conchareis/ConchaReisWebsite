8845e99a68ab429e1b2daefd9d0c7f15049bc585
Concha Reis <conchardr@gmail.com>
2023-04-01 12:08:15 +0100

    Updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-// import Header from '../partials/Header';
+import Header from "../partials/Header";
 import HeroHome from "../partials/HeroHome";
-// import FeaturesHome from '../partials/Features';
-// import FeaturesBlocks from '../partials/FeaturesBlocks';
-// import Testimonials from '../partials/Testimonials';
-// import Newsletter from '../partials/Newsletter';
-// import Footer from '../partials/Footer';
-// import Banner from '../partials/Banner';
+import FeaturesHome from "../partials/Features";
+import FeaturesBlocks from "../partials/FeaturesBlocks";
+import Testimonials from "../partials/Testimonials";
+import Newsletter from "../partials/Newsletter";
+import Footer from "../partials/Footer";
+import Banner from "../partials/Banner";
 
 function Home() {
   return (
@@ -19,16 +19,16 @@ function Home() {
       <main className="flex-grow">
         {/*  Page sections */}
         <HeroHome />
-        {/* <FeaturesHome /> */}
-        {/* <FeaturesBlocks /> */}
-        {/* <Testimonials /> */}
-        {/* <Newsletter /> */}
+        <FeaturesHome />
+        <FeaturesBlocks />
+        <Testimonials />
+        <Newsletter />
       </main>
 
-      {/* <Banner /> */}
+      <Banner />
 
       {/*  Site footer */}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
